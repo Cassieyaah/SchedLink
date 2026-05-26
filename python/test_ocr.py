3667a5d95d2c8b4f5908cb8614fb09d588e377a0
Bryron Lim <limbryron13@gmail.com>
2026-05-26 18:24:45 +0800

    ocrocrocrocr

--- python/test_ocr.py
+++ python/test_ocr.py
@@ -1,5 +1,7 @@
 import cv2
 import pytesseract
+import json
+import sys
 from PIL import Image
 from cleanUp import clean_ocr_text, merge_multiline_schedules
 from parser import parse_schedule
@@ -10,10 +12,13 @@ pytesseract.pytesseract.tesseract_cmd = (
 )
 
 # Load image
-img = cv2.imread(r"C:\xampp\htdocs\SchedLink\uploads\sample.png")
+image_path = sys.argv[1]
+
+img = cv2.imread(image_path)
 
 # Convert to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 
 # Enlarge image
 gray = cv2.resize(gray, None, fx=2, fy=2)
@@ -29,6 +34,13 @@ text = pytesseract.image_to_string(thresh, config=custom_config)
 
 cleaned_text = clean_ocr_text(text)
 merged_schedules = merge_multiline_schedules(cleaned_text)
+parsed_schedules = []
+
 for sched in merged_schedules:
+
     parsed = parse_schedule(sched)
-    print(parsed)
+
+    if parsed:
+        parsed_schedules.append(parsed)
+
+print(json.dumps(parsed_schedules, indent=4))
